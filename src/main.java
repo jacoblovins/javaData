a64d5dbb606c144bcccbe0f3fdc93718c2e0c0a3
jacoblovins <lovins.jacob@yahoo.com>
2021-05-20 07:58:45 -0400

    initial commit

---
+++ src/main.java
@@ -0,0 +1,18 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int[] nums = new int[10];
+		
+		for(int i = 0; i < 10; ++i) {
+			nums[i] = i;
+		}
+			
+		for(int i = 0; i < 10; ++i) {
+			System.out.println(nums[i]);
+		}
+
+	}
+
+}
