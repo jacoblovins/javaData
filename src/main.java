0e083dc4aea4d7ad4f063650653423b4487bf953
jacoblovins <lovins.jacob@yahoo.com>
2021-05-20 08:06:39 -0400

    arrays linear search

--- src/main.java
+++ src/main.java
@@ -8,10 +8,23 @@ public class main {
 		for(int i = 0; i < 10; ++i) {
 			nums[i] = i;
 		}
+
+//		we know the index of the item we want to manipulate
+		nums[0] = 100;   // O(1)
+		
+//		If we do not know the index of the item
+//		Linear search O(n)
+		for(int i = 0; i < 10; ++i) {
+			if(nums[i] == 6) {
+				System.out.println("We have found the item at index: " + i);
+			}
+		}
+		
 			
 		for(int i = 0; i < 10; ++i) {
 			System.out.println(nums[i]);
 		}
+		
 
 	}
 
